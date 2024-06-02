5f4bc8bbda400e73d93ea19198729f9481d275c2
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2024-06-02 16:50:35 -0400

    feat: config cmp of codenium

--- lua/plugins/codenium.lua
+++ lua/plugins/codenium.lua
@@ -1,4 +1,20 @@
 return {
-  "Exafunction/codeium.vim",
-  event = "BufEnter",
+  "nvim-cmp",
+  dependencies = {
+    -- codeium
+    {
+      "Exafunction/codeium.nvim",
+      cmd = "Codeium",
+      build = ":Codeium Auth",
+      opts = {},
+    },
+  },
+
+  opts = function(_, opts)
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+    })
+  end,
 }
